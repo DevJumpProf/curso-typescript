1942ef5b8d482cee67c74909e5d2dfb109da38fa
Arbusta <gabriel.munoz@arbusta.net>
2022-08-22 16:05:07 -0300

    clase1

---
+++ ts-project/src/03-demo3.ts
@@ -0,0 +1,10 @@
+let productPrice  = 123;
+let productName = "Product 1";
+
+
+(()=>{
+
+  let productPrice  = 123;
+  let productName = "Product 1";
+
+  })();
---
+++ ts-project/src/03-typing.ts
@@ -0,0 +1,15 @@
+
+(()=>{
+
+
+let productPrice  = 123;
+let productName = "Product 1";
+
+/* productName. */
+
+
+const myProductStock = 1000;
+
+const productName2 = "Product 2";
+
+})();
